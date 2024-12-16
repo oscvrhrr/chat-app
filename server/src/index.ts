2b3727ed227cfc6792118e4a81769cd0e2ac9752
Oscar Herrera <a.oscarherrera@gmail.com>
2024-12-15 21:33:59 -0600

    project setup

---
+++ server/src/index.ts
@@ -0,0 +1,22 @@
+import express, { Express } from "express"
+
+
+const app: Express = express()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(4001, () => {
+  console.log("app running on port 4001")
+})
