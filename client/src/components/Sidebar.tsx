3e6a028596d60fa540c85a2c4de7799d7115b5cd
Oscar Herrera <a.oscarherrera@gmail.com>
2025-01-23 10:59:28 -0600

    installed icons, axios & created the sidebar

---
+++ client/src/components/Sidebar.tsx
@@ -0,0 +1,28 @@
+import { Button } from "@radix-ui/themes"
+import { ChatBubbleIcon, MagnifyingGlassIcon, ExitIcon, GearIcon, GlobeIcon } from "@radix-ui/react-icons"
+
+export const Sidebar = () => {
+  return (
+    <div className="text-white flex flex-col justify-between h-screen w-36 p-3">
+      <div className="flex flex-col pl-1">
+        <Button className="hover:bg-dark-mauve-600 text-dark-mauve-1100"  variant="ghost" size="3">
+          <ChatBubbleIcon/>Chats
+        </Button>
+        <Button className="hover:bg-dark-mauve-600 text-dark-mauve-1100 my-1" variant="ghost" size="3">
+          <GlobeIcon/>Groups
+        </Button>
+        <Button className="hover:bg-dark-mauve-600 text-dark-mauve-1100" variant="ghost" size="3">
+          <MagnifyingGlassIcon/>Search
+        </Button>
+      </div>
+      <div className="flex flex-col pl-1">
+        <Button className="hover:bg-dark-mauve-600 text-dark-mauve-1100 my-1" variant="ghost" size="3">
+          <GearIcon/>Settings
+        </Button>
+        <Button className="hover:bg-dark-mauve-600 text-dark-mauve-1100" variant="ghost" size="3">
+          <ExitIcon/>Logout
+        </Button>  
+      </div>
+    </div>
+  )
+}
--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,10 +1,11 @@
-
+import { Sidebar } from "../components/Sidebar"
 
 export const Dashboard = () => {
   return (
-   <>
-    <p>this is the dashboard</p>
+   <div className="bg-dark-mauve-200 h-screen">
+    <Sidebar/>
 
-   </>
+
+   </div>
   )
 }
