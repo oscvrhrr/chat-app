56a5bc7584aed753a6a2e1cf04e2b5817222dcf6
Oscar Herrera <a.oscarherrera@gmail.com>
2025-01-31 13:14:51 -0600

    fixed Node ENV to MODE to access frontend enviroment

--- client/src/config/config.ts
+++ client/src/config/config.ts
@@ -1,3 +1,3 @@
-const baseURL = "production" === process.env.NODE_ENV ? import.meta.env.VITE_PROD_BASE_URL : import.meta.env.VITE_DEV_BASE_URL;
+const baseURL = "production" === import.meta.env.MODE ? import.meta.env.VITE_PROD_BASE_URL : import.meta.env.VITE_DEV_BASE_URL;
 
 export default baseURL;
