f862c47e3f333105573e9c9010bc092999104cf4
Oscar Herrera <a.oscarherrera@gmail.com>
2025-02-24 14:53:27 -0600

    feat: allowed users to register

--- client/src/components/AuthForm.tsx
+++ client/src/components/AuthForm.tsx
@@ -4,8 +4,14 @@ import axios from "axios";
 import { useNavigate } from "react-router";
 import baseURL from "../config/config";
 
+interface AuthFormProps {
+  title: string;
+  segment: string;
+  toggle: () => void;
+}
 
-export const Login = () => {
+
+export const AuthForm = ({ title, segment, toggle }: AuthFormProps) => {
   const [inputValues, setInputValues] = useState({fullname: "", email: "", password: ""});
   const navigate = useNavigate();
 
@@ -18,21 +24,27 @@ export const Login = () => {
   const login = async(event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     try {
+      const data: { fullname?: string; email: string; password: string } = {
+        email: inputValues.email,
+        password: inputValues.password,
+      };
+
+      if (title === "Sign Up") {
+        data.fullname = inputValues.fullname;
+      }
+
       const response = await axios({
         method: "POST",
-        url: `${baseURL}/auth/login`,
-        data: {
-          email: inputValues.email,
-          password: inputValues.password
-        },
+        url: `${baseURL}/auth/${segment}`,
+        data,
         headers: {
-          "Content-Type": "application/json"
-        }
+          "Content-Type": "application/json",
+        },
       });
-      localStorage.setItem("token", response.data.token)
-      navigate("/dashboard")
+      localStorage.setItem("token", response.data.token);
+      navigate("/dashboard");
     } catch (err) {
-      console.log(err)
+      console.log(err);
     }
    
   }
@@ -41,7 +53,37 @@ export const Login = () => {
 
   return (
     <>
-      <Form.Root onSubmit={ login } className="w-[330px] h-[400px] border border-dark-mauve-700 p-9 rounded-lg bg-dark-mauve-300">      
+      <Form.Root
+        onSubmit={login}
+        className="w-[330px] h-[400px] border border-dark-mauve-700 p-9 rounded-lg bg-dark-mauve-300"
+      >
+        <h2 className="text-center text-2xl font-bold">{ title }</h2>
+        { title === "Sign Up" 
+          && 
+        (  <Form.Field className="mb-2.5 grid" name="fullname">
+          <div className="flex items-baseline justify-between">
+            <Form.Label className="text-[15px] font-medium leading-[35px] text-white">
+              Fullname
+            </Form.Label>
+            <Form.Message
+              className="text-[13px] text-white opacity-80"
+              match="valueMissing"
+            >
+              Please provide your fullname
+            </Form.Message>
+          </div>
+          <Form.Control asChild>
+            <input
+              className="box-border bg-dark-mauve-200 inline-flex h-[35px] w-full appearance-none items-center justify-center rounded bg-blackA2 px-2.5 text-[15px] leading-none text-white shadow-[0_0_0_1px] shadow-blackA6 outline-none selection:bg-blackA6 selection:text-white hover:shadow-[0_0_0_1px_black] focus:shadow-[0_0_0_2px_black]"
+              placeholder="Enter your fullname"
+              type="text"
+              required
+              onChange={handleInputValues}
+              value={inputValues.fullname}
+            />
+          </Form.Control>
+        </Form.Field>)
+        }
         <Form.Field className="mb-2.5 grid" name="email">
           <div className="flex items-baseline justify-between">
             <Form.Label className="text-[15px] font-medium leading-[35px] text-white">
@@ -66,7 +108,8 @@ export const Login = () => {
               placeholder="Enter your email address"
               type="email"
               required
-              onChange={ handleInputValues }
+              onChange={handleInputValues}
+              value={inputValues.email}
             />
           </Form.Control>
         </Form.Field>
@@ -88,16 +131,20 @@ export const Login = () => {
               placeholder="Enter your password"
               type="password"
               required
-              onChange={ handleInputValues }
+              onChange={handleInputValues}
+              value={inputValues.password}
             />
           </Form.Control>
         </Form.Field>
         <Form.Submit asChild>
           <button className="mt-2.5 bg-dark-mauve-1000 text-white box-border inline-flex h-[35px] w-full items-center justify-center rounded px-[15px] font-medium leading-none text-violet11  hover:bg-mauve3 focus:shadow-[0_0_0_2px] focus:shadow-black focus:outline-none">
-            Login
+            {title}
           </button>
         </Form.Submit>
       </Form.Root>
+      <p className="cursor-pointer" onClick={toggle}>
+        { title === 'Sign Up' ? "Already have an account? Login here!" : "Don't have an account? Register here!" }
+      </p>
     </>
   );
 };
--- client/src/pages/Landing.tsx
+++ client/src/pages/Landing.tsx
@@ -1,10 +1,34 @@
-import { Login } from "../components/Login";
-
+import { AuthForm } from "../components/AuthForm";
+import { useState } from "react";
 export const Landing = () => {
+  const [isRegistered, setIsRegistered] = useState(true);
+
+
+  const handleToggle = (isToggled: boolean) => {
+    setIsRegistered(isToggled);
+  }
+
+
+
   return (
     <div className="bg-dark-mauve-200 h-screen flex flex-col justify-center items-center">
       <h1 className="text-2xl font-bold text-white">Chat App</h1>
-      <Login/>
+      {
+        !isRegistered
+        ?
+        <AuthForm
+          title="Sign Up"
+          segment="signup"
+          toggle={() => handleToggle(true)}
+
+        />
+        :
+        <AuthForm
+          title="Login"
+          segment="login"
+          toggle={() => handleToggle(false)}
+        />
+      }
     </div>
   );
 };
